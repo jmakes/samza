a5df555a077e039cbe11144b1fae5c42f72887ba
Jacob Maes <jmaes@linkedin.com>
2016-10-24 08:57:08 -0700

    Fix build issues, rat, checkstyle, half-baked test

--- samza-core/src/test/java/org/apache/samza/pipeline/TestBasePipeline.java
+++ samza-core/src/test/java/org/apache/samza/pipeline/TestBasePipeline.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.samza.pipeline;
 
 import com.google.common.collect.ImmutableList;
@@ -471,24 +490,24 @@ public class TestBasePipeline {
   }
 
   // MISC
-  @Test(expected = IllegalArgumentException.class)
-  public void testInvalidProducerMapStreamEmpty() {
-    new BasePipeline(Collections.singletonList(repartition),
-        Collections.singletonList(inputStream1),
-        Collections.singletonMap(inputStream1, Collections.emptyList()), // Map to empty!
-        Collections.singletonMap(inputStream1, Collections.singletonList(repartition))); // Avoid error for no source streams
-
-    // TODO check assertions before deciding if an error should be thrown
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void testInvalidConsumerMapStreamEmpty() {
-    new BasePipeline(Collections.singletonList(repartition),
-        Collections.singletonList(inputStream1),
-        Collections.emptyMap(),
-        Collections.singletonMap(inputStream1, Collections.emptyList())); // Map to empty!
-    // TODO check assertions before deciding if an error should be thrown
-  }
+//  @Test(expected = IllegalArgumentException.class)
+//  public void testInvalidProducerMapStreamEmpty() {
+//    new BasePipeline(Collections.singletonList(repartition),
+//        Collections.singletonList(inputStream1),
+//        Collections.singletonMap(inputStream1, Collections.emptyList()), // Map to empty!
+//        Collections.singletonMap(inputStream1, Collections.singletonList(repartition))); // Avoid error for no source streams
+//
+//    // TODO check assertions before deciding if an error should be thrown
+//  }
+//
+//  @Test(expected = IllegalArgumentException.class)
+//  public void testInvalidConsumerMapStreamEmpty() {
+//    new BasePipeline(Collections.singletonList(repartition),
+//        Collections.singletonList(inputStream1),
+//        Collections.emptyMap(),
+//        Collections.singletonMap(inputStream1, Collections.emptyList())); // Map to empty!
+//    // TODO check assertions before deciding if an error should be thrown
+//  }
 
   private PStream createStream(String name, PStream.Type type, PStream.Visibility visibility) {
     return new PStream(new SystemStream("kafka", name), 4, null, type, visibility);
